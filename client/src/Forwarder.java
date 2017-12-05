0872117c1b2c1fa57467f41741a8a2787462bedf
Troy Madsen <madsentr@eos17.cis.gvsu.edu>
2017-12-05 13:30:15 -0500

    -Forwarder speed fixes

--- client/src/Forwarder.java
+++ client/src/Forwarder.java
@@ -11,14 +11,18 @@ public class Forwarder extends Thread {
 	private ArrayList<ClientHandler> clients;
 	
 	/** List of updaes to forward to clients */
-	private List<String> updates;
+	private LinkedList<String> updates;
+
+	/** List of prospective changes to updates */
+	private List<String> prospective;
 	
 	/**
 	 * Creates a Forwarder object
 	 */
 	public Forwarder() {
 		clients = new ArrayList<>();
-		updates = Collections.synchronizedList(new LinkedList<String>());
+		updates = new LinkedList<>();
+		prospective = Collections.synchronizedList(new LinkedList<String>());
 	}
 	
 	/**
@@ -45,7 +49,7 @@ public class Forwarder extends Thread {
 	 * @param update String of the update to forward to clients
 	 */
 	public synchronized void addUpdate(String update) {
-		updates.add(update);
+		prospective.add(update);
 	}
 	
 	/**
@@ -53,18 +57,21 @@ public class Forwarder extends Thread {
 	 */
 	public void run() {
 		while (true) {
-			// Wait for the there to be updates to send
-			while (updates.size() == 0);
+			// Add any new updates
+			updates.addAll(prospective);
+			prospective.clear();
 
-			String update = updates.get(0) + "\n";
-			updates.remove(0);
+			// Forward all updates
+			while (updates.size() > 0) {
+				String update = updates.pop() + "\n";
 
-			for (ListIterator<ClientHandler> iter = clients.listIterator(); iter.hasNext();) {
-				try {
-					System.out.println("SENDING:\t" + update);
-					iter.next().getOutputStream().writeBytes(update);
-				} catch (Exception e) {
-					// Fail quietly
+				for (ListIterator<ClientHandler> iter = clients.listIterator(); iter.hasNext();) {
+					try {
+						System.out.println("SENDING:\t" + update);
+						iter.next().getOutputStream().writeBytes(update);
+					} catch (Exception e) {
+						// Fail quietly
+					}
 				}
 			}
 		}
