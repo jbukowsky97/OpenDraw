88584ef825386ed448f2b06db08b6332fdab19b8
Sam Carson <carsonsa@mail.gvsu.edu>
2017-12-05 03:54:16 -0500

    Client drawing GUI

--- client/src/ClientGUI.java
+++ client/src/ClientGUI.java
@@ -1,19 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
 
-public class ClientGUI extends JFrame {
+public class ClientGUI extends JFrame{
 
+    private JButton hostBtn;
+    private JButton connectBtn;
+    private JButton clearBtn;
+    private JButton colorBtn;
 
-    JButton hostBtn;
+    private Color color;
 
-    JButton connectBtn;
+    private DrawPanel drawPanel;
 
-
+    private Client client;
 
     public ClientGUI(){
 
+        color = Color.BLACK;
+        client = new Client(this);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
         this.setSize(1000,500);
         JPanel contentPane = new JPanel();
         contentPane.setLayout(new GridBagLayout());
@@ -22,13 +34,110 @@ public class ClientGUI extends JFrame {
 
 
         JPanel buttonPanel = new JPanel();
-        DrawPanel draw = new DrawPanel();
+        buttonPanel.setLayout(new GridLayout(4, 1));
+        drawPanel = new DrawPanel(client);
 
         c.fill = GridBagConstraints.BOTH;
         c.gridheight = 1;
         c.gridwidth = 4;
-        c.weightx = 1;
+        c.weightx = .8;
+        c.weighty = 1;
+        contentPane.add(drawPanel, c);
 
+        hostBtn = new JButton("Host");
+        connectBtn = new JButton("Connect");
+        clearBtn = new JButton("Clear");
+        colorBtn = new JButton("Choose Color");
+
+        buttonPanel.add(hostBtn);
+        buttonPanel.add(connectBtn);
+        buttonPanel.add(clearBtn);
+        buttonPanel.add(colorBtn);
+
+        c.fill = GridBagConstraints.BOTH;
+        c.gridheight = 1;
+        c.gridwidth = 1;
+        c.weightx = .2;
+        c.weighty = 1;
+        contentPane.add(buttonPanel, c);
+
+        hostBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //create text fields
+                JTextField ipText = new JTextField();
+                JTextField portText = new JTextField();
+
+                //create message for optionpane
+                Object[] message = {"Server ip: ", ipText,
+                        "Server Port: ", portText
+                };
+
+                //open optionpane and set result to n
+                int n = JOptionPane.showConfirmDialog(null,
+                        message, "Create Server",
+                        JOptionPane.OK_CANCEL_OPTION);
+
+                //create serverInfo object if OK is pressed
+                if (n == JOptionPane.OK_OPTION) {
+                    String ip = ipText.getText();
+                    int port = Integer.parseInt(portText.getText());
+                }
+            }
+        });
+
+        connectBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //create text fields
+                JTextField ipText = new JTextField();
+                JTextField portText = new JTextField();
+
+                //create message for optionpane
+                Object[] message = {"Server ip: ", ipText,
+                        "Server Port: ", portText
+                };
+
+                //open optionpane and set result to n
+                int n = JOptionPane.showConfirmDialog(null,
+                        message, "Join Server",
+                        JOptionPane.OK_CANCEL_OPTION);
+
+                //create serverInfo object if OK is pressed
+                if (n == JOptionPane.OK_OPTION) {
+                    String ip = ipText.getText();
+                    int port = Integer.parseInt(portText.getText());
+
+                    try {
+                        client.connect(ip, port);
+                        connectBtn.setEnabled(false);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            }
+        });
+
+        clearBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+
+        colorBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                color = JColorChooser.showDialog(null, "Choose a color", color);
+                drawPanel.updateColor(color);
+            }
+        });
+
+
+        this.add(contentPane);
+        this.setVisible(true);
 
 
 
@@ -38,16 +147,11 @@ public class ClientGUI extends JFrame {
     }
 
     public void process(String command) {
-
+        drawPanel.processCommand(command);
     }
 
 
     public static void main(String[] args){
         ClientGUI gui = new ClientGUI();
     }
-
-
-
-
-
 }
